27d6a6514cd85f5c6f8a8200320d05b8cb95209a
Léncy <87382565+lencydev@users.noreply.github.com>
2021-09-24 21:52:44 +0400

    Update ready.js

--- lencys-bot/src/events/ready.js
+++ lencys-bot/src/events/ready.js
@@ -1,7 +1,5 @@
 const ctx = new (require('../interfaces/context.js'));
 
-const restartingModel = require('../models/restart.js')
-
 const chalk = require('chalk');
 const moment = require('moment-timezone');
 
@@ -12,8 +10,6 @@ module.exports = {
 
   run: async (client) => {
 
-    await restartingModel.updateOne({ _id: '6143310c7674da034c2abc68' }, { status: true }).catch((error) => console.log(error));
-
     setTimeout(() => {
   
       console.log(`${chalk.bold.hex('#FFFFFF')(`${moment().tz('Europe/Istanbul').format('hh:mm:ss')}`)}\n\n  Guilds:       ${chalk.hex('#FFFFFF')(`${client.guilds.cache.size.toLocaleString(undefined, { minimumFractionDigits: 0 })}`)}\n  Users:        ${chalk.hex('#FFFFFF')(`${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString(undefined, { minimumFractionDigits: 0 })}`)}\n  Channels:     ${chalk.hex('#FFFFFF')(`${(client.channels.cache.filter(c => c.type == 'GUILD_TEXT').size + client.channels.cache.filter(c => c.type == 'GUILD_VOICE').size).toLocaleString(undefined, { minimumFractionDigits: 0 })}`)}\n\n  Commands:     ${chalk.hex('#FFFFFF')(`${client.commands.size}`)}\n\n  Node.js:      ${chalk.hex('#FFFFFF')(`${process.version.replace(`v`, ``)}`)}\n  Discord.js:   ${chalk.hex('#FFFFFF')(`${require('discord.js').version}`)}\n`);
@@ -23,7 +19,6 @@ module.exports = {
   
       await console.log(`${ctx.config.log.time} The reboot is complete.`);
   
-      await restartingModel.updateOne({ _id: '6143310c7674da034c2abc68' }, { status: false }).catch((error) => console.log(error));
     }, 60000);
   },
 };
