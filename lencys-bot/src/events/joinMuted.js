c3e6020e1d4f3cc7db11cf182f0b31e6e79ea17c
Léncy <87382565+lencydev@users.noreply.github.com>
2021-10-02 14:27:23 +0400

    fix

--- lencys-bot/src/events/joinMuted.js
+++ lencys-bot/src/events/joinMuted.js
@@ -1,19 +1,19 @@
 const ctx = new (require('../interfaces/context.js'));
 
 module.exports = {
-  
+
   name: 'guildMemberAdd',
   disabled: false,
 
   run: async (client, member) => {
-  
+
     let fetch = await ctx.model.mute.findOne({ guild: member.guild.id, user: member.id });
     let muted = member.guild.roles.cache.find((role) => role.name == 'Muted');
 
     if (!fetch) return;
-    
+
     if ((Date.now() <= fetch.endDate) || fetch) {
-      
+
       if (!muted) return await ctx.model.mute.findOne({ guild: member.guild.id, user: member.id }).deleteOne().catch((error) => console.log(error));
       if (member.roles.highest.position >= member.guild.me.roles.highest.position) return await ctx.model.mute.findOne({ guild: member.guild.id, user: member.id }).deleteOne().catch((error) => console.log(error));
       if (muted.position >= member.guild.me.roles.highest.position) return await ctx.model.mute.findOne({ guild: member.guild.id, user: member.id }).deleteOne().catch((error) => console.log(error));
@@ -27,7 +27,7 @@ module.exports = {
         if (muted.position >= member.guild.me.roles.highest.position) return await ctx.model.mute.findOne({ guild: member.guild.id, user: member.id }).deleteOne().catch((error) => console.log(error));
 
         await ctx.model.mute.findOne({ guild: member.guild.id, user: member.id }).deleteOne().catch((error) => console.log(error));
-        
+
         await member.roles.remove(muted);
 
       }, fetch.endDate - Date.now());
